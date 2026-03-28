9297d8c0361d9c098d488687f7c5bb8250bc1d21
PanchoGamer <jorgesaa36@gmail.com>
2026-03-28 15:31:13 -0300

    Add Menu

--- Taller/Logica/Main.java
+++ Taller/Logica/Main.java
@@ -1,8 +1,35 @@
 package Logica;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+
+		int option = 0;
+
+		do {
+			System.out.println("1) Menu de usuarios");
+			System.out.println("2) Menu de analisis");
+			System.out.println("3) Salir");
+			System.out.print("> ");
+			String entrada = s.nextLine();
+
+			
+			try {
+				option = Integer.parseInt(entrada);
+
+			} catch (Exception e) {
+				System.out.println("\nPor favor ingrese solo numeros. \n");
+			}
+			
+			if (option > 3 || option < 1) {
+				System.out.println("\nError, elija entre los numeros mostrados \n");
+			}
+			
+		} while (option != 3);
 		
+		s.close();
 	}
 
 }
