96d8adbb9f4188f03568d2ab8b779faba31675eb
PanchoGamer <jorgesaa36@gmail.com>
2026-04-02 22:53:47 -0300

    Add User menu and some corrections

--- Taller/Logica/Main.java
+++ Taller/Logica/Main.java
@@ -1,88 +1,97 @@
+/// Jorge Saavedra Sanchez 
+/// 22.347.590-6 
+/// Ingenieria en Tecnologias de Informacion
+
 package Logica;
 
-import java.util.Scanner;
-
+// Importamos librerias necesarias
+import java.util.*;
 import java.io.*;
 
 public class Main {
 
-	static String[] usuarios = new String[20];
-	static String[] contras = new String[20];
-
-	public static void leerArchivo() {
-		File file = new File("txts/Usuarios.txt");
-
-		Scanner su = new Scanner(file);
-
-		int x = 0;
-		int y = 0;
-
-		while (su.hasNextLine()) {
-
-			String line = su.nextLine();
-			String[] partes = line.split(";");
-
-			String usua = partes[0];
-			String contrase = partes[1];
-
-			usuarios[x] = usua;
-			contras[y] = contrase;
-
-			x++;
-			y++;
-
-		}
-		su.close();
-
-	}
-
 	public static void main(String[] args) {
+
+		// Se seleccionas los archivos necesarios
+		File fileU = new File("txts/Usuarios.txt");
+		File fileR = new File("txts/Registros.txt");
+
+		// Se crea un Scanner para la lectura del teclado
 		Scanner s = new Scanner(System.in);
-
-		leerArchivo();
-
-		for (int i = 0; i < 2; i++) {
-			System.out.println(usuarios[i]);
-			System.out.println(contras[i]);
-
-		}
+		// Scanner sArchR = new Scanner(fileR);
 
 		int option = 0;
-		String usuario = ("");
-		String contra = ("");
-
 		do {
-			System.out.println("1) Menu de usuarios");
+
+			// Menu :D
+			System.out.println("1) Menu de Usuarios");
 			System.out.println("2) Menu de analisis");
 			System.out.println("3) Salir");
-			System.out.print("> ");
+
+			System.out.print(">");
 			String entrada = s.nextLine();
 
-			try {
-				option = Integer.parseInt(entrada);
+			// Transformacion de string a int
+			option = Integer.parseInt(entrada);
 
-			} catch (Exception e) {
-				System.out.println("\nPor favor ingrese solo numeros. \n");
-			}
-
-			if (option > 3 || option < 1) {
-				System.out.println("\nError, elija entre los numeros mostrados \n");
-			}
-
+			// Menu de Usuario
 			if (option == 1) {
-				do {
-					System.out.print("Usuario: \n");
-					usuario = s.nextLine();
-					System.out.print("\nContraseña: ");
-					contra = s.nextLine();
 
-				} while (usuario == "Hola" && contra == "Papus");
+				// Pregunta usuario y contraseña para luego verificar con el TXT correspondiente
+				System.out.print("Usuario: ");
+				String Usuario = s.nextLine();
+				System.out.print("Contraseña: ");
+				String Contra = s.nextLine();
 
+				try {
+					// Scanner para el usuario
+					Scanner sArchU = new Scanner(fileU);
+
+					while (sArchU.hasNextLine()) {
+						String line = sArchU.nextLine();
+						String[] partes = line.split(";");
+
+						int opcion = 0;
+						System.out.println(partes[0]);
+						System.out.println(partes[1]);
+
+						if (Usuario == partes[0] && Contra == partes[1]) {
+							do {
+
+								try {
+									System.out.printf("Bienvenido %s \n", Usuario);
+									System.out.println("Que deseas realizar? \n");
+									System.out.println("1) Registrar actividad");
+									System.out.println("2) Modificar actividad");
+									System.out.println("3) Eliminar actividad");
+									System.out.println("4) Cambiar contraseña");
+									System.out.println("5) Salir");
+
+									String entry = s.nextLine();
+
+									opcion = Integer.parseInt(entry);
+
+								} catch (InputMismatchException e) {
+									System.out.println("Formato incorrecto, vuelva a ingresar");
+								}
+
+							} while (opcion != 5);
+
+						} else {
+							System.out.println("Datos Incorrectos");
+						}
+
+					}
+
+				} catch (Exception e) {
+					System.out.println("no existe el archivo");
+				}
 			}
 
 		} while (option != 3);
 
 		s.close();
+
 	}
 
 }
