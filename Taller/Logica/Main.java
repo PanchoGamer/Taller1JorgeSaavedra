0312580ec267a73466b75b4a4bef2dd23910d845
PanchoGamer <jorgesaa36@gmail.com>
2026-04-03 18:43:21 -0300

    Add funtion add activies

--- Taller/Logica/Main.java
+++ Taller/Logica/Main.java
@@ -15,12 +15,15 @@ public class Main {
 		// Se seleccionas los archivos necesarios
 		File fileU = new File("txts/Usuarios.txt");
 		File fileR = new File("txts/Registros.txt");
+		
+		String[] Usua = new String[3];
 
 		// Se crea un Scanner para la lectura del teclado
 		Scanner s = new Scanner(System.in);
 		// Scanner sArchR = new Scanner(fileR);
 
 		int option = 0;
+		int w = 0;
 		do {
 
 			// Menu :D
@@ -33,7 +36,9 @@ public class Main {
 
 			// Transformacion de string a int
 			option = Integer.parseInt(entrada);
-
+			
+			int c = 0;
+			
 			// Menu de Usuario
 			if (option == 1) {
 
@@ -47,6 +52,7 @@ public class Main {
 				
 				int opcion = 0;
 				
+				
 				try {
 					// Scanner para el usuario
 					Scanner sArchU = new Scanner(fileU);
@@ -54,6 +60,8 @@ public class Main {
 					while (sArchU.hasNextLine()) {
 						String line = sArchU.nextLine();
 						String[] partes = line.split(";");
+						
+						Usua[c] = partes[0];
 
 						if (Usuario.equals(partes[0])) {
 							if (Contra.equals(partes[1])) {
@@ -61,14 +69,15 @@ public class Main {
 							}
 							
 						}
+						c++;
 
 					}
 					if (pass) {
+						System.out.println("\nAcceso Correcto!");
+						System.out.printf("\nBienvenido %s\n", Usuario);
 						do {
 							
 							try {
-								System.out.println("\nAcceso Correcto!");
-								System.out.printf("\nBienvenido %s\n", Usuario);
 								System.out.println("\nQue deseas realizar? \n");
 								System.out.println("1) Registrar actividad");
 								System.out.println("2) Modificar actividad");
@@ -81,8 +90,35 @@ public class Main {
 								
 								opcion = Integer.parseInt(entry);
 								
-							} catch (InputMismatchException e) {
+							} catch (Exception e) {
 								System.out.println("Formato incorrecto, vuelva a ingresar");
+							}
+							
+							if (opcion == 1) {
+								try {
+									FileWriter fw = new FileWriter(fileR, true);
+									BufferedWriter bw = new BufferedWriter(fw);
+									
+									System.out.print("Ingrese fecha (DD/MM/AAAA): ");
+									String fecha = s.nextLine();
+									System.out.print("\nIngrese horas:");
+									String horas = s.nextLine();
+									System.out.print("\nIngrese actividad: ");
+									String actividad = s.nextLine();
+									
+									String ladd = (Usuario + ";" + fecha + ";" + horas + ";" + actividad);
+
+									bw.newLine();
+									bw.write(ladd);
+									
+									bw.close();
+									
+									
+								} catch (IOException e) {
+									System.out.println("Error con Registros.txt");
+								}
+								
+								
 							}
 							
 						} while (opcion != 5);
@@ -97,33 +133,43 @@ public class Main {
 					System.out.println("no existe el archivo Usuarios.txt");
 				}
 			}
-			else if (option == 2){
-				System.out.println("Bienvenido al ");
+			else if (option == 2) {
 				int opcion = 0;
-				try {
-					Scanner sArchR = new Scanner(fileR);
+				do {
+					System.out.printf("\nBienvenido al menu de analisis!");
+					System.out.println("\nQue deseas realizar? \n");
+					System.out.println("1) Actividad más realizada");
+					System.out.println("2) Actividad más realizada por cada usuario");
+					System.out.println("3) Usuario con mayor procastinacion");
+					System.out.println("4) Ver todas las actividades");
+					System.out.println("5) Salir");
+					System.out.print(">");
 					
-					do {
-						while (sArchR.hasNextLine()) {
-							String line = sArchR.nextLine();
-							System.out.println(line);
-							
-							}
-						try {
-							String entry = s.nextLine();
-							
-							option = Integer.parseInt(entry);
-							
-						} catch (InputMismatchException e) {
-							System.out.println();
-						}
+					String entry = s.nextLine();
+					
+					try {
+						opcion = Integer.parseInt(entry);
 						
-					} while (opcion != 5);
+					} catch(Exception e) {
+						System.out.println("Formato Incorrecto, vuelva presionar para volver al menu");
+						entry = s.nextLine();
+					}
 					
-					sArchR.close();
-				} catch (Exception e) {
-					System.out.println("No existe el archivo Registros.txt");
-				}
+					if (opcion == 4) {
+						try {
+							Scanner sArchR = new Scanner(fileR);
+							
+							while (sArchR.hasNextLine()) {
+								String linea = sArchR.nextLine();
+								System.out.println(linea);
+							}
+							sArchR.close();
+						} catch(Exception e) {
+							System.out.println("No existe el archivo");
+						}
+					}
+					
+				} while (opcion != 5);
 			}
 		} while (option != 3);
 
